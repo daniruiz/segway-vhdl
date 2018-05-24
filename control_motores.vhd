161bbf377d94a4c870d91d728a8a82ee18f84fca
Daniel Ruiz de Alegría <daniruizdealegria@gmail.com>
2018-05-24 03:30:29 +0200

    Primer commit

---
+++ control_motores.vhd
@@ -0,0 +1,22 @@
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity control_motores is
+    Port ();
+
+end control_motores;
+
+architecture Comportamiento of decodificador_mpu6050 is
+
+    
+begin
+    process(reloj)
+    begin
+        if (reloj'event and reloj='1') then
+
+        end if;
+    end process;
+end Comportamiento;
